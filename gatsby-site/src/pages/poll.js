c5f3817a72c9df770e0159ee95cc62384800e11f
Isaac Kang <isaac.kang@consensys.net>
2019-11-05 16:41:36 -0500

    add form submission
    - set provider
    - set metamask account
    - validate percentages
    - sign message

--- gatsby-site/src/pages/poll.js
+++ gatsby-site/src/pages/poll.js
@@ -1,6 +1,9 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { providers } from 'ethers';
 import styled from 'styled-components';
 import { layout, space } from 'styled-system';
+import panUtils from 'panvala-utils';
+import sum from 'lodash/sum';
 
 import Box from '../components/system/Box';
 import Layout from '../components/Layout';
@@ -12,36 +15,42 @@ import pollTwo from '../img/poll-2.png';
 
 const categories = [
   {
+    categoryID: 1,
     title: 'Ethereum 2.0',
     previous: 34,
     description:
       'These grants fund work that scales the base layer of the Ethereum network by implementing the Ethereum 2.0 roadmap. Past Panvala grant recipients in this category are Prysmatic Labs, Sigma Prime, Nimbus, and Whiteblock. Other teams in our community that do this kind of work are ChainSafe and Harmony.',
   },
   {
+    categoryID: 2,
     title: 'Layer 2 Scaling',
     previous: 5,
     description:
       'These grants fund work that scale Ethereum without modifying the base layer. Past Panvala grant recipients in this category are Connext, Counterfactual, Plasma Group, and Prototypal. Other teams in our community that do this kind of work are LeapDAO, OmiseGo, and Raiden.',
   },
   {
+    categoryID: 3,
     title: 'Security',
     previous: 16,
     description:
       'These grants fund work that make it easier to build and run Ethereum applications that perform their intended functions without bugs or security flaws. Past Panvala grant recipients in this category are Level K, ConsenSys Diligence, Runtime Verification, and Dapphub. Other teams in our community that do this kind of work are Zeppelin, Trail of Bits, and Quantstamp.',
   },
   {
+    categoryID: 4,
     title: 'Developer Tools and Growth',
     previous: 4,
     description:
       'These grants fund work that increase the productivity of Ethereum developers, and make it easier for new developers to get started so we can reach One Million Developers in 2020. Past Panvala grant recipients in this category are ethers.js, Asseth, and Tenderly. Other teams in our community that do this kind of work are Truffle, Embark, and Cryptoeconomics.study.',
   },
   {
+    categoryID: 5,
     title: 'Dapps and Usability',
     previous: 4,
     description:
       'These grants fund work that produces Ethereum-based applications, games, and user experience improvements that bring more users to Ethereum. Past Panvala grant recipients in this category are BrightID, Gnosis, and Bounties Network. Other teams in our community that do this kind of work are MetaCartel DAO, Axie Infinity, Burner Wallet and Universal Login.',
   },
   {
+    categoryID: 6,
     title: 'Panvala',
     previous: 37,
     description:
@@ -57,6 +66,17 @@ const ClipContainer = styled.div`
 `;
 const Poll = () => {
   const pollFormRef = useRef(null);
+  const [account, setAccount] = useState('');
+  const [provider, setProvider] = useState();
+  const [allocations, setAllocations] = useState();
+  const [percentages, setPercentages] = useState({
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+    6: '',
+  });
 
   useEffect(() => {
     if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
@@ -74,18 +94,131 @@ const Poll = () => {
     });
   }
 
-  function handleChange(event) {
-    const { value, name } = event.target;
-    console.log(value, name);
+  async function setSelectedAccount() {
+    let selectedAccount = (await provider.listAccounts())[0];
+    // user not enabled for this app
+    if (!selectedAccount) {
+      try {
+        selectedAccount = (await window.ethereum.enable())[0];
+      } catch (error) {
+        if (error.stack.includes('User denied account authorization')) {
+          alert('MetaMask not enabled. In order to donate pan, you must authorize this app.');
+        }
+      }
+    }
+    await setAccount(selectedAccount);
   }
 
-  function handleFormSubmit(event) {
+  useEffect(() => {
+    if (
+      typeof window !== 'undefined' &&
+      typeof window.ethereum !== 'undefined' &&
+      typeof provider !== 'undefined'
+    ) {
+      setSelectedAccount();
+    }
+  }, [provider]);
+
+  async function connectWallet() {
+    if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+      if (typeof provider === 'undefined') {
+        const p = new providers.Web3Provider(window.ethereum);
+        await setProvider(p);
+      } else if (!account) {
+        await setSelectedAccount();
+      }
+    } else {
+      alert('MetaMask not found. Please download MetaMask @ metamask.io');
+    }
+  }
+
+  async function postPoll() {
+    const allocations = percentages;
+    function generateMessage(account, pollID) {
+      // Always use checksum address in the message
+      return `Response from ${account} for poll ID ${pollID}`;
+    }
+
+    console.log('panUtils:', panUtils);
+    const pollID = '1';
+    const message = await generateMessage(account, pollID);
+
+    const signer = provider.getSigner();
+    const signature = await signer.signMessage(message);
+    const data = {
+      response: {
+        account,
+        allocations,
+      },
+      signature,
+    };
+    console.log('data:', data);
+    return;
+
+    // const apiHost = 'https://localhost:5001';
+    // const endpoint = `${apiHost}/api/polls/${pollID}`;
+
+    // fetch(endpoint, {
+    //   method: 'POST',
+    //   body: JSON.stringify(data),
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //     'Access-Control-Allow-Origin': apiHost,
+    //     'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    //     'Access-Control-Allow-Headers': 'Origin, Content-Type',
+    //   },
+    // });
+  }
+
+  useEffect(() => {
+    if (account && allocations) {
+      console.log('form valid');
+      postPoll();
+    }
+  }, [account, allocations]);
+
+  function handleChange(value, categoryID) {
+    setPercentages({
+      ...percentages,
+      [categoryID]: value,
+    });
+  }
+
+  async function handleFormSubmit(event) {
     event.preventDefault();
-    console.log('submit', event);
-  }
+    if (!provider) {
+      await connectWallet();
+    }
+    const percentValues = Object.keys(percentages);
 
-  function connectWallet() {
-    console.log('clickex');
+    // Create a new array of invalid percentages (0 - 100)
+    const zeroToHundred = /^[1-9][0-9]?$|^100$/;
+    const invalidPercentages = percentValues.reduce((acc, val) => {
+      if (zeroToHundred.test(percentages[val])) {
+        return acc;
+      }
+      return [...acc, { [val]: percentages[val] }];
+    }, []);
+    console.log('invalidPercentages:', invalidPercentages);
+
+    // Calculate the sum of the percentages
+    const totalPercentage = sum(percentValues.map(p => parseInt(percentages[p])));
+    console.log('totalPercentage:', totalPercentage);
+
+    // Valid percentages
+    if (invalidPercentages.length === 0 && totalPercentage === 100) {
+      // Format allocations
+      const allocations = categories.map((c, index) => {
+        return {
+          categoryID: c.categoryID,
+          points: parseInt(percentages[index]),
+        };
+      });
+
+      // console.log('allocations:', allocations);
+      setAllocations(allocations);
+    }
   }
 
   return (
@@ -134,7 +267,10 @@ const Poll = () => {
             PAN tokens have been granted to teams that the whole Ethereum community depends on. The
             more tokens you acquire to vote, the more work those teams can fund with their tokens.
           </p>
-          <Button handleClick={connectWallet} text="Connect your wallet" />
+          <Button
+            handleClick={connectWallet}
+            text={account ? 'Wallet connected!' : 'Connect your wallet'}
+          />
         </div>
       </section>
 
@@ -154,7 +290,7 @@ const Poll = () => {
                 const identifier = `poll-points-category-${index}`;
 
                 return (
-                  <div key={index} className="cf pa3 bb bw-2 b--black-10">
+                  <div key={identifier} className="cf pa3 bb bw-2 b--black-10">
                     <div className="fl w-80 pa2 pr4">
                       <div className="f4 b">{title}</div>
                       <p>{description}</p>
@@ -172,7 +308,8 @@ const Poll = () => {
                           id={identifier}
                           required
                           placeholder="%"
-                          onChange={handleChange}
+                          onChange={e => handleChange(e.target.value, category.categoryID)}
+                          value={percentages[category.categoryID]}
                           className="f6 input-reset b--black-10 pv3 ph2 db w-100 br3 mt2 tr"
                         />
                       </div>
