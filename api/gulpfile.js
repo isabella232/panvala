7b1cc04fd166d5b8998257ae7359415e8296bd12
Isaac Kang <isaac.kang@consensys.net>
2019-12-06 17:41:18 -0500

    watch changes

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -1,4 +1,4 @@
-const { series, src, dest } = require('gulp');
+const { series, src, dest, watch } = require('gulp');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
 
@@ -22,3 +22,42 @@ function tsc(cb) {
 }
 
 exports.default = series(tsc, copyJSON);
+
+// function watcher(done) {
+//   series(tsc, copyJSON)
+//   watch('./src/**/*', () => series(tsc, copyJSON));
+//   done();
+// }
+
+// exports.watch = watcher;
+
+// // version ^3.9.1
+// gulp.task('compileTS', function(done) {
+//   gulp
+//     .src('./src/**/*')
+//     .pipe(tsProject())
+//     .pipe(gulp.dest('./dist/'));
+//   done();
+// });
+
+// gulp.task('watch', ['compileTS'], function() {
+//   // gulp.watch('./src/**/*', ['compileTS']);
+// });
+
+// gulp.task('default', ['compileTS']);
+
+// version ^4.0.2
+// function copyJSON(cb) {
+//   cb();
+//   return src('./src/**/*.json').pipe(dest('./dist/'));
+// }
+
+// gulp.task('compileTS', function() {
+//   return gulp
+//     .src('./src/**/*')
+//     .pipe(tsProject())
+//     .pipe(gulp.dest('./dist/'));
+// });
+
+// gulp.task('watch', gulp.series('compileTS'))
+// gulp.task('default', gulp.series('compileTS'))
